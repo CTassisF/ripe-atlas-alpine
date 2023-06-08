1cb6a77f2fe295610e13976ef2a338b722343d52
César de Tassis Filho <ctassisf@gmail.com>
2023-06-08 20:03:08 -0300

    Enable provenance attestations.

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 docker buildx rm --force ripe-atlas-alpine
 docker buildx create --name ripe-atlas-alpine --use
-docker buildx build --no-cache --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64/v8 --provenance=false --pull --push --tag ctassisf/ripe-atlas-alpine:latest .
+docker buildx build --no-cache --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64/v8 --provenance=mode=max --pull --push --tag ctassisf/ripe-atlas-alpine:latest .
 docker buildx rm --force ripe-atlas-alpine
