b1dc0308bf0592466eaf6550f8262aca2b9b87ff
César de Tassis Filho <ctassisf@gmail.com>
2026-08-14 15:20:41 -0300

    Tag images with Software Probe version.

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-docker buildx rm --force ripe-atlas-alpine
+set -e
+
+VERSION=$(curl -fsSL https://raw.githubusercontent.com/RIPE-NCC/ripe-atlas-software-probe/master/VERSION)
+
+docker buildx rm --force ripe-atlas-alpine || true
 docker buildx create --name ripe-atlas-alpine --use
-docker buildx build --no-cache --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64/v8,linux/ppc64le,linux/riscv64,linux/s390x --provenance=mode=max --sbom=true --pull --push --tag ctassisf/ripe-atlas-alpine:latest .
+docker buildx build --no-cache --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64/v8,linux/ppc64le,linux/riscv64,linux/s390x --provenance=mode=max --sbom=true --pull --push --tag ctassisf/ripe-atlas-alpine:latest --tag ctassisf/ripe-atlas-alpine:"$VERSION" .
 docker buildx rm --force ripe-atlas-alpine
